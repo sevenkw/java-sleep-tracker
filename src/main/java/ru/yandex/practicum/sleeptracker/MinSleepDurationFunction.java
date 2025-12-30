6e595c4e45e2ebe16c9e39f46bc27c0ae8b0f8c0
yaroslavanferov <yaroslavanferov@yandex.ru>
2025-12-30 16:14:33 +0300

    happy new year!

--- src/main/java/ru/yandex/practicum/sleeptracker/MinSleepDurationFunction.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/MinSleepDurationFunction.java
@@ -7,6 +7,6 @@ public class MinSleepDurationFunction implements SleepFunction {
     @Override
     public SleepAnalysisResult apply(List<SleepingSession> sessions) {
         long min = sessions.stream().mapToLong(s -> s.getDurationMinutes()).min().orElse(0);
-        return new SleepAnalysisResult("Минимальная длительность сессии сна (мин) " , min);
+        return new SleepAnalysisResult("Минимальная длительность сессии сна (мин) ", min);
     }
 }
